9c0bd4230800362f01ac64eea5fb691eaba536bf
RECEP ŞEN <me@senrecep.com>
2021-05-17 01:25:09 +0300

    PageDirector

---
+++ Clients/GeneralStockMarket.BlazorServerAppClient/ComponentBases/PageDirector.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace GeneralStockMarket.BlazorServerAppClient.ComponentBases
+{
+    public class PageDirector : ComponentBase
+    {
+        [Parameter]
+        public string Url { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        protected override void OnAfterRender(bool firstRender)
+        {
+            NavigationManager.NavigateTo(Url);
+        }
+     
+    }
+}
